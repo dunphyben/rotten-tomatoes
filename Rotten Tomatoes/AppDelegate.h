5358240184854726de46a4f953be030767dbfaba
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-07-19 13:33:27 -0700

    Initial Commit

---
+++ Rotten Tomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rotten Tomatoes
+//
+//  Created by Benjamin Dunphy on 7/19/14.
+//  Copyright (c) 2014 Benjamin Dunphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
