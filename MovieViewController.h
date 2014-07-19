77cef2cbff971a4fb5651349a17c77ca22536add
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-07-19 13:35:39 -0700

    Add MovieViewController with xib

---
+++ MovieViewController.h
@@ -0,0 +1,13 @@
+//
+//  MovieViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Benjamin Dunphy on 7/19/14.
+//  Copyright (c) 2014 Benjamin Dunphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieViewController : UIViewController
+
+@end
