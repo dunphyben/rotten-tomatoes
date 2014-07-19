031f3c43921538d9a5d32c3c84e9911cc8d3949c
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-07-19 14:48:55 -0700

    Create MovieCellTableViewCell file with xib file

---
+++ MovieCellTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MovieCellTableViewCell.h
+//  Rotten Tomatoes
+//
+//  Created by Benjamin Dunphy on 7/19/14.
+//  Copyright (c) 2014 Benjamin Dunphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieCellTableViewCell : UITableViewCell
+
+@end
---
+++ MovieCellTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  MovieCellTableViewCell.m
+//  Rotten Tomatoes
+//
+//  Created by Benjamin Dunphy on 7/19/14.
+//  Copyright (c) 2014 Benjamin Dunphy. All rights reserved.
+//
+
+#import "MovieCellTableViewCell.h"
+
+@implementation MovieCellTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
