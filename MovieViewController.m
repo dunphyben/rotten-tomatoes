0cf72de2e1b1ff80ad1c95f8ba99b42b46159a75
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-07-19 14:33:01 -0700

    Add table view methods and json url to .m file

--- MovieViewController.m
+++ MovieViewController.m
@@ -27,8 +27,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    NSString *url = @"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=duxgbnqmvd37e5m7xdv8w9xw%22";
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+                              [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError * connectionError) {
+        id object = [NSJSONSerialization JSONObjectWithData: data options:0 error:nil];
+        //self.movies = object[@"movies"];
+        [self.tableView reloadData];
+                              }];
+        
     // Do any additional setup after loading the view from its nib.
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    cell.textLabel.text = @"Hello";
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -37,3 +59,6 @@
 }
 
 @end
+
+
+
